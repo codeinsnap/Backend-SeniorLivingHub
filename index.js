5685a31ad4fa6c3ac925caf8670e35ef606bf8c1
yogesh sharma <yog192325@gmail.com>
2023-11-22 22:18:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path = require('path');
+const jwt = require("jsonwebtoken");
+const cors = require('cors');
+
+const connectDB = require('./server/database/connection');
+
+const app = express();
+
+app.use(cors());
+
+dotenv.config( { path : 'config.env'} )
+const PORT = process.env.PORT || 8080
+
+// log requests
+app.use(morgan('tiny'));
+
+// mongodb connection
+connectDB();
+
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended : false}) ,  bodyparser.json())
+
+// load routers
+app.use('/', require('./server/routes/router'))
+
+//Json 
+app.use(express.json());
+
+app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
